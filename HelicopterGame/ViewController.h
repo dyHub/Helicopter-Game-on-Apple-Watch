5956c2e280fea700bdd53edcce85a7af7f3dff98
dyHub <dy_joy2012@126.com>
2015-12-21 14:01:07 -0800

    Initial Commit

---
+++ HelicopterGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelicopterGame
+//
+//  Created by seab on 12/21/15.
+//  Copyright © 2015 Yuhan Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelicopterGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelicopterGame
+//
+//  Created by seab on 12/21/15.
+//  Copyright © 2015 Yuhan Dai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
